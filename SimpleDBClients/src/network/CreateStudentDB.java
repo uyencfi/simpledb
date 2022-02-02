d4312f5d47c267fe319f8177c0eda6741cd98118
jodichoo <jodi.choo@gmail.com>
2022-02-02 20:48:58 +0800

    Create index on enroll table

--- SimpleDBClients/src/network/CreateStudentDB.java
+++ SimpleDBClients/src/network/CreateStudentDB.java
@@ -14,13 +14,9 @@ public class CreateStudentDB {
          stmt.executeUpdate(s);
          System.out.println("Table STUDENT created.");
          
-//         s = "create index idxMajor on student(majorid) using hash"; 
-//         stmt.executeUpdate(s); 
-//         System.out.println("index idxMajor created on student(majorid) hash");
-//         
-//         s = "create index idxMajor on student(sname) using btree"; 
-//         stmt.executeUpdate(s); 
-//         System.out.println("index idxMajor created on student(sname) btree");
+         s = "create index idxMajor on student(majorid) using hash"; 
+         stmt.executeUpdate(s); 
+         System.out.println("index idxMajor created on student(majorid) hash");
 
          s = "insert into STUDENT(SId, SName, MajorId, GradYear) values ";
          String[] studvals = {"(1, 'joe', 10, 2021)",
@@ -80,6 +76,10 @@ public class CreateStudentDB {
          s = "create table ENROLL(EId int, StudentId int, SectionId int, Grade varchar(2))";
          stmt.executeUpdate(s);
          System.out.println("Table ENROLL created.");
+         
+         s = "create index idxStudent on enroll(studentid) using btree"; 
+         stmt.executeUpdate(s); 
+         System.out.println("index idxStudent created on enroll(studentid) btree");
 
          s = "insert into ENROLL(EId, StudentId, SectionId, Grade) values ";
          String[] enrollvals = {"(14, 1, 13, 'A')",
